92513fad2c54159cf6ab2751939b3e87c013558e
Daniel Nuñez Romero <daninuro96@gmail.com>
2023-12-18 14:46:44 +0100

    Add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './view/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    
+
+
+    { path: 'error', component: ErrorComponent },
+    { path: '**', redirectTo: '/error' }
+
+];
---
+++ src/app/view/error/error.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.css'
+})
+export class ErrorComponent {
+
+  constructor(private location: Location) {}
+  goBack() {
+    this.location.back();
+
+}
+}
